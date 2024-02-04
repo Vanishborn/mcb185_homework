443de4928dd074aac7d39099be90cf3e1da0f304
Vanishborn <henrylihaoran@outlook.com>
2024-02-03 16:08:13 -0800

    space to tabs
    fixed space indent to tab indent

--- 32fibonacci.py
+++ 32fibonacci.py
@@ -17,12 +17,12 @@
 # 	fibi_1 = fibi
 
 def fibonacci(x):
-    x1 = -1
-    x2 = 1
-    for i in range(x):
-        total = x1 + x2
-        x1 = x2
-        x2 = total
-        print(total)
+	x1 = -1
+	x2 = 1
+	for i in range(x):
+		total = x1 + x2
+		x1 = x2
+		x2 = total
+		print(total)
 
 fibonacci(9)
